91cddd859caa7f7fd05cb5a1e97833cc28c54cc3
vijay kumar <59472284+vijaybirju@users.noreply.github.com>
2023-03-27 14:03:07 +0530

    Exception file modify

--- src/exception.py
+++ src/exception.py
@@ -6,3 +6,15 @@ def error_massage_details(error,error_details:sys):
     error_massage="Error occured in phton script name [{0}] line number [{1}] error massage[{2}]".format(
     file_name,exc_tb.tb_lineno,str(error)
     )
+
+    return error_massage
+
+
+class CustomException(Exception):
+    def __init__(self,error_massage,error_details:sys):
+        super.__init__(error_massage)
+        self.error_massage=error_massage_details(error_massage,error_details=error_details)
+
+
+    def __str__(self):
+        return self.error_massage
