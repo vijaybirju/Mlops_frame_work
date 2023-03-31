dddb8918128142365645d88548fbd1c5521d8da9
vijay kumar <59472284+vijaybirju@users.noreply.github.com>
2023-03-31 15:52:26 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -23,16 +23,23 @@ def save_objects(file_path,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            # model.fit(X_train,y_train)
 
             logging.info(f'model : {model}')
 
-            model.fit(X_train,y_train) 
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
