d628a721e5dde1f22bde1296963d69a386977f61
vijay kumar <59472284+vijaybirju@users.noreply.github.com>
2023-04-03 16:20:57 +0530

    Lower python version for campatiblity

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str) ->List[str]:
-    """
-    This will return the list of requirements
-
-    """
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
-    with open(file_path) as file_opj:
-        requirements = file_opj.readline()
-        requirements = [req.replace('\n','') for req in requirements]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
-
 setup(
-    name='MLops',
-    version='0.0.1',
-    author='Vijay',
-    author_email='Vijaykumar14198@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+name='mlproject',
+version='0.0.1',
+author='vijay',
+author_email='vijaykumar14198@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
